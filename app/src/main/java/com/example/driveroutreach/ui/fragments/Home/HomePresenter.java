25739cceba13d17a389eefdca08b80b8a4474540
Nour AL-Borno <nour.alborno98@gmail.com>
2023-04-17 18:56:37 +0300

    Home Fragment xml

--- app/src/main/java/com/example/driveroutreach/ui/fragments/Home/HomePresenter.java
+++ app/src/main/java/com/example/driveroutreach/ui/fragments/Home/HomePresenter.java
@@ -1,4 +1,15 @@
 package com.example.driveroutreach.ui.fragments.Home;
 
+import androidx.fragment.app.Fragment;
+
 public class HomePresenter {
+    HomeView view;
+
+    public HomePresenter(HomeView view) {
+        this.view = view;
+    }
+
+    public void SettingMap(Fragment fragment){
+        view.onSetMapFrag(fragment);
+    }
 }
