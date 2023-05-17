e25c59c6088f604da6f5001d96bc1bb0038ec1b6
taghreed louz <taghreedlouz7@gmail.com>
2023-05-17 17:20:26 +0300

    edite login

--- app/src/main/java/com/example/driveroutreach/ui/activities/Login/LoginActivity.java
+++ app/src/main/java/com/example/driveroutreach/ui/activities/Login/LoginActivity.java
@@ -28,18 +28,6 @@ import java.util.concurrent.TimeUnit;
 public class LoginActivity extends AppCompatActivity {
 
     ActivityLoginBinding binding;
-    private String mVerificationId;
-    private PhoneAuthProvider.ForceResendingToken mResendToken;
-
-    @Override
-    protected void onStart() {
-        super.onStart();
-        FirebaseUser currentUser = FirebaseAuth.getInstance().getCurrentUser();
-        if (currentUser != null) {
-            startActivity(new Intent(getBaseContext(), MainActivity.class));
-            finish();
-        }
-    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -47,155 +35,9 @@ public class LoginActivity extends AppCompatActivity {
         binding = ActivityLoginBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-
-
-        binding.btnLogin.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                if (TextUtils.isEmpty(binding.etMobile.getText().toString())) {
-                    binding.etMobile.setError("Enter your mobile number");
-                    binding.btnLogin.setEnabled(true);
-                    return;
-                } else {
-                    binding.progressBar.setVisibility(View.VISIBLE);
-                    binding.btnLogin.setText(R.string.sending);
-                    binding.etMobile.setEnabled(false);
-                    binding.btnLogin.setEnabled(false);
-
-                    //num
-                    // String mobileNumber = binding.etMobile.getText().toString().trim();
-
-
-
-
-//                    FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
-//                    FirebaseAuthSettings firebaseAuthSettings = firebaseAuth.getFirebaseAuthSettings();
-//
-//                    String mobileNumber = binding.etMobile.getText().toString().trim();
-//
-//                    PhoneAuthOptions options =
-//                            PhoneAuthOptions.newBuilder(firebaseAuth)
-//                                    .setPhoneNumber("+970" +mobileNumber)       // Phone number to verify
-//                                    .setTimeout(60L, TimeUnit.SECONDS) // Timeout and unit
-//                                    .setActivity(LllllActivity.this);               // Activity (for callback binding)
-//                                    .setCallbacks(new PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
-//                                        @Override
-//                                        public void onVerificationCompleted(@NonNull PhoneAuthCredential phoneAuthCredential) {
-//                                            Log.d("LLLTAG", "onVerificationCompleted   :    failed");
-//                                        }
-//
-//                                        @Override
-//                                        public void onVerificationFailed(@NonNull FirebaseException e) {
-//                                            Toast.makeText(LllllActivity.this, e.getMessage(), Toast.LENGTH_SHORT).show();
-//                                            Log.d("LLLTAG", "onVerificationFailed   :  " + e.getMessage());
-//                                        }
-//
-//                                        @Override
-//                                        public void onCodeSent(@NonNull String verificationId, @NonNull PhoneAuthProvider.ForceResendingToken forceResendingToken) {
-//                                            // super.onCodeSent(verificationId, forceResendingToken);
-//                                            Log.d("LLLTAG", "onCodeSent   :  ");
-//
-//                                            // intent
-//                                            Intent intent = new Intent(getBaseContext(), VvvvvActivity.class);
-//                                            intent.putExtra("mobile", mobileNumber);
-//                                            intent.putExtra("verificationId", verificationId);
-//                                            startActivity(intent);
-//                                        }
-//                                    })          // OnVerificationStateChangedCallbacks
-//                                    .build();
-//
-//                    if (firebaseAuthSettings.isAppVerificationDisabledForTesting()) {
-//                        PhoneAuthProvider.verifyPhoneNumber(options);
-//                    } else {
-//                        firebaseAuth.setLanguageCode("en"); // or any other language code
-//                        PhoneAuthProvider.verifyPhoneNumber(options);
-//                    }
-
-
-
-
-                    FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
-                    FirebaseAuthSettings firebaseAuthSettings = firebaseAuth.getFirebaseAuthSettings();
-                    firebaseAuthSettings.setAppVerificationDisabledForTesting(false);
-
-                    String mobileNumber = binding.etMobile.getText().toString().trim();
-                    PhoneAuthOptions.Builder builder = PhoneAuthOptions.newBuilder(firebaseAuth);
-                    builder.setPhoneNumber("+970" + mobileNumber);
-                    builder.setTimeout(60L, TimeUnit.SECONDS);
-                    builder.setActivity(LoginActivity.this);
-                    builder.setCallbacks(new PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
-                        @Override
-                        public void onVerificationCompleted(@NonNull PhoneAuthCredential phoneAuthCredential) {
-                            Log.d("LLLTAG", "onVerificationCompleted   :    failed");
-                        }
-
-                        @Override
-                        public void onVerificationFailed(@NonNull FirebaseException e) {
-                            Toast.makeText(LoginActivity.this, e.getMessage(), Toast.LENGTH_SHORT).show();
-                            Log.d("LLLTAG", "onVerificationFailed   :  " + e.getMessage());
-                            setEnabledVisibility();
-                        }
-
-                        @Override
-                        public void onCodeSent(@NonNull String verificationId, @NonNull PhoneAuthProvider.ForceResendingToken forceResendingToken) {
-                            super.onCodeSent(verificationId, forceResendingToken);
-                            Log.d("LLLTAG", "onCode8Sent   :  "+verificationId);
-                            setEnabledVisibility();
-                            // intent
-                            Intent intent = new Intent(getBaseContext(), VerificationActivity.class);
-                            intent.putExtra("mobile", mobileNumber);
-                            intent.putExtra("verificationId", verificationId);
-                            startActivity(intent);
-                        }
-                    });
-                    PhoneAuthOptions options = builder
-                            .build();
-
-                    firebaseAuth.setLanguageCode("en"); // or any other language code
-                    PhoneAuthProvider.verifyPhoneNumber(options);
-
-
-
-
-
-//                    PhoneAuthProvider.getInstance().verifyPhoneNumber(
-//                            "+970" + mobileNumber,
-//                            60,
-//                            TimeUnit.SECONDS,
-//                            LllllActivity.this,
-//                            new PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
-//                                @Override
-//                                public void onVerificationCompleted(@NonNull PhoneAuthCredential phoneAuthCredential) {
-//                                    Log.d("LLLTAG", "onVerificationCompleted   :    failed");
-//                                }
-//
-//                                @Override
-//                                public void onVerificationFailed(@NonNull FirebaseException e) {
-//                                    Toast.makeText(LllllActivity.this, e.getMessage(), Toast.LENGTH_SHORT).show();
-//                                    Log.d("LLLTAG", "onVerificationFailed   :  " + e.getMessage());
-//                                    setEnabledVisibility();
-//                                }
-//
-//                                @Override
-//                                public void onCodeSent(@NonNull String verificationId, @NonNull PhoneAuthProvider.ForceResendingToken forceResendingToken) {
-//                                    super.onCodeSent(verificationId, forceResendingToken);
-//                                    mVerificationId = verificationId;
-//                                    mResendToken = forceResendingToken;
-//                                    Log.d("LLLTAG", "onCodeSent   :  ");
-//
-//                                    // intent
-//                                    Intent intent = new Intent(getBaseContext(), VvvvvActivity.class);
-//                                    intent.putExtra("mobile", mobileNumber);
-//                                    intent.putExtra("verificationId", verificationId);
-//                                    startActivity(intent);
-//                                    setEnabledVisibility();
-//                                }
-//                            }
-//                    );
-                }
-            }
-        });
-    }
+binding.btnLogin.setOnClickListener(view -> {
+    sendCodeVerification();
+});}
 
     @Override
     public void onBackPressed() {
@@ -207,5 +49,47 @@ public class LoginActivity extends AppCompatActivity {
         binding.btnLogin.setText(R.string.send);
         binding.etMobile.setEnabled(true);
         binding.btnLogin.setEnabled(true);
+    }
+    private void sendCodeVerification() {
+        String phone = binding.etMobile.getText().toString().trim();
+        Log.e("Phone", phone);
+        if (TextUtils.isEmpty(phone)) {
+            Toast.makeText(this, "Enter your phone", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        if (phone.startsWith("0")) {
+            phone = phone.substring(1);
+        }
+        binding.progressBar.setVisibility(View.VISIBLE);
+
+        PhoneAuthProvider.getInstance().verifyPhoneNumber(
+                "+970" + phone,
+                60,
+                TimeUnit.SECONDS,
+                this,
+                new PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
+                    @Override
+                    public void onVerificationCompleted(@NonNull PhoneAuthCredential phoneAuthCredential) {
+                        Log.e("FirebaseException", "Verification completed");
+                    }
+
+                    @Override
+                    public void onVerificationFailed(@NonNull FirebaseException e) {
+                        Log.e("FirebaseException", e.toString());
+                    }
+
+                    @Override
+                    public void onCodeSent(@NonNull String verificationId, @NonNull PhoneAuthProvider.ForceResendingToken token) {
+                        super.onCodeSent(verificationId, token);
+                        binding.progressBar.setVisibility(View.GONE);
+                        Intent intent = new Intent(LoginActivity.this, VerificationActivity.class);
+                        intent.putExtra("verificationId", verificationId);
+                        intent.putExtra("resendingToken", token);
+                        startActivity(intent);
+                        finish();
+                    }
+                }
+        );
     }
 }
