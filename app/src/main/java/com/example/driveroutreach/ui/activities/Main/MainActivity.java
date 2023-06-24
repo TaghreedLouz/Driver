292ca759001560a5be66997aeb2931142d9499c6
Taghreed Louz <121046503+TaghreedLouz@users.noreply.github.com>
2023-06-24 17:00:26 +0300

    show dialog

--- app/src/main/java/com/example/driveroutreach/ui/activities/Main/MainActivity.java
+++ app/src/main/java/com/example/driveroutreach/ui/activities/Main/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.driveroutreach.ui.activities.Main;
 
 import android.Manifest;
 import android.app.Dialog;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentSender;
 import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
 import android.location.LocationManager;
@@ -26,9 +28,17 @@ import com.example.driveroutreach.ui.fragments.Home.HomeFragment;
 import com.example.driveroutreach.ui.fragments.schedule.ScheduleFragment;
 import com.example.driveroutreach.ui.fragments.schedule.days.DayFragment;
 import com.firebase.geofire.GeoFire;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.common.api.ResolvableApiException;
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationCallback;
 import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.LocationSettingsRequest;
+import com.google.android.gms.location.LocationSettingsResponse;
+import com.google.android.gms.location.LocationSettingsStatusCodes;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.bottomsheet.BottomSheetBehavior;
 import com.google.android.material.navigation.NavigationBarView;
 import com.google.firebase.database.DatabaseReference;
@@ -39,27 +49,10 @@ public class MainActivity extends AppCompatActivity implements MainView, DayFrag
     BottomSheetBehavior bottomSheetBehavior;
     Dialog dialog;
     Button btn_getLocation;
-    public static final int REQUEST_LOCATION = 1;
-    LocationManager locationManager;
-    Float latitude_sp, longitude_sp;
-    public final String LATITUDE_KEY = "latitude";
-    public final String LONGITUDE_KEY = "longitude";
-    AlertDialog alertDialog;
-    double longitude_driver;
-    double latitude_driver ;
-    int LOCATION_REQUEST_CODE = 10001;
-    FusedLocationProviderClient fusedLocationClient;
+    private LocationManager locationManager;
     SharedPreferences sp;
     SharedPreferences.Editor edit;
-
-    private LocationRequest locationRequest;
-    private static final String DIALOG_SHOWN_KEY = "dialog_shown";
-    LocationCallback locationCallback;
-    DatabaseReference ref ;
     public final String DRIVER_ID_KEY = "driverId";
-    GeoFire geoFire;
-
-    boolean isClicked = false;
     private static final int PERMISSIONS_REQUEST_LOCATION = 1001;
 
     @Override
@@ -73,6 +66,7 @@ public class MainActivity extends AppCompatActivity implements MainView, DayFrag
 
 
 
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
 
 
         String driverId= sp.getString(DRIVER_ID_KEY,null);
@@ -89,15 +83,15 @@ public class MainActivity extends AppCompatActivity implements MainView, DayFrag
                 return true;
             }
         });
-        showLocationDialog();
 
 
-//        if (isClicked){
-//            showLocationDialog();
-//
-//        }else {
-//            startService(new Intent(MainActivity.this, LocationService.class));
-//        }
+//        showLocationDialog();
+
+        if (ActivityCompat.checkSelfPermission(MainActivity.this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            startLocationService();
+        } else {
+            showLocationDialog();
+        }
 
     }
 
@@ -123,10 +117,11 @@ public class MainActivity extends AppCompatActivity implements MainView, DayFrag
 
 
     private void showLocationDialog() {
+
+
         dialog = new Dialog(MainActivity.this);
-        View dialogView = LayoutInflater.from(getApplicationContext()).inflate(R.layout.item_dialog, findViewById(R.id.custom_dialog));
+        View dialogView = LayoutInflater.from(getApplicationContext()).inflate(R.layout.item_dialog, null);
         dialog.setContentView(dialogView);
-        dialog.show();
 
         btn_getLocation = dialogView.findViewById(R.id.btn_getLocation);
         btn_getLocation.setOnClickListener(new View.OnClickListener() {
@@ -135,15 +130,24 @@ public class MainActivity extends AppCompatActivity implements MainView, DayFrag
 
                 // ااذا اخد البيرمشن ولا لا
                 if (ActivityCompat.checkSelfPermission(MainActivity.this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
-
                     startLocationService();
-
                 } else {
                     ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, PERMISSIONS_REQUEST_LOCATION);
+                    dismissDialog();
                 }
 
             }
         });
+
+        dialog.show();
+    }
+
+    private void dismissDialog() {
+        if (dialog != null && dialog.isShowing()) {
+            dialog.dismiss();
+        }
+
+
 
     }
 
@@ -155,7 +159,56 @@ public class MainActivity extends AppCompatActivity implements MainView, DayFrag
                 startLocationService();
             } else {
                 Toast.makeText(this, "Location permission denied", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
 
+
+
+    private void turnOnGPS() {
+        LocationRequest locationRequest = LocationRequest.create()
+                .setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+
+        LocationSettingsRequest.Builder builder = new LocationSettingsRequest.Builder()
+                .addLocationRequest(locationRequest);
+        builder.setAlwaysShow(true);
+
+        Task<LocationSettingsResponse> result = LocationServices.getSettingsClient(this)
+                .checkLocationSettings(builder.build());
+
+        result.addOnCompleteListener(task -> {
+            try {
+                LocationSettingsResponse response = task.getResult(ApiException.class);
+                // تم تفعيل نظام تحديد المواقع بنجاح
+                Toast.makeText(MainActivity.this, "Location enabled successfully", Toast.LENGTH_SHORT).show();
+            } catch (ApiException e) {
+                switch (e.getStatusCode()) {
+                    case LocationSettingsStatusCodes.RESOLUTION_REQUIRED:
+                        try {
+                            ResolvableApiException resolvableApiException = (ResolvableApiException) e;
+                            resolvableApiException.startResolutionForResult(MainActivity.this, 2);
+                        } catch (IntentSender.SendIntentException ex) {
+                            ex.printStackTrace();
+                        }
+                        break;
+                    case LocationSettingsStatusCodes.SETTINGS_CHANGE_UNAVAILABLE:
+                        // هي لما يكون الجهاز ما بيدعم تحيد اللوكيشن
+                        break;
+                }
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 2) {
+            if (resultCode == RESULT_OK) {
+                // تم تفعيل Location
+                Toast.makeText(this, "Location enabled successfully", Toast.LENGTH_SHORT).show();
+            } else if (resultCode == RESULT_CANCELED) {
+                // تم إلغاء تفعيل Location
+                Toast.makeText(this, "Location is deactivated", Toast.LENGTH_SHORT).show();
             }
         }
     }
@@ -164,8 +217,7 @@ public class MainActivity extends AppCompatActivity implements MainView, DayFrag
     private void startLocationService() {
         //يشغل كود السيرفس
         startService(new Intent(MainActivity.this, LocationService.class));
-        dialog.dismiss();
-        isClicked = true;
+        turnOnGPS();
     }
 
 }
