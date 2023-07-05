1b6c3568c32fda83e5a8a99fabaf68783003015d
Nour AL-Borno <nour.alborno98@gmail.com>
2023-07-05 18:48:42 +0300

    Archeive child Adpater

---
+++ app/src/main/java/com/example/driveroutreach/adapters/NotificationAdapter.java
@@ -0,0 +1,17 @@
+package com.example.driveroutreach.adapters;
+
+import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class NotificationAdapter {
+
+
+    class NotVH extends RecyclerView.ViewHolder{
+
+        public NotVH(@NonNull View itemView) {
+            super(itemView);
+        }
+    }
+}
